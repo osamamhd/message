c7fbb358ec50b061ad113ea3fd1a388073023c9e
osamamhd <osamamhd82@gmail.com>
2021-07-06 23:36:22 +0200

    ⚡️ get and add messages.

--- main.js
+++ main.js
@@ -2,6 +2,29 @@ const list = document.querySelector('ul');
 const form = document.querySelector('form');
 const button = document.querySelector('button');
 
+// add message
+
+const addMessage = (message, id) => {
+    let time = dateFns.distanceInWordsToNow(
+        message.created_at.toDate(),
+        { addSuffix: true }
+    );
+    let html = `
+        <li data-id="${id}" class="border border-gray-600 outline-none focus:outline-none rounded p-2 my-2">
+            <div class="flex justify-between border-b border-gray-400 font-medium">
+                <p>${message.name}</p>
+                <p>${time}</p>
+                
+            </div>
+            <div>
+                <p class="py-1 word-break">${message.body}</p>
+            </div>
+        </li>
+    `
+    list.innerHTML += html;
+}
+
+
 // post message 
 form.addEventListener('submit', e => {
     e.preventDefault();
@@ -15,7 +38,18 @@ form.addEventListener('submit', e => {
 
     db.collection('messages').add(message).then(() => {
         console.log('message added')
+        form.reset();
     }).catch(err => {
         console.log(err)
+    })
+})
+
+// get messages
+const getMessages = db.collection('messages').onSnapshot(snapshot => {
+    snapshot.docChanges().forEach(change => {
+        const doc = change.doc;
+        if(change.type === 'added'){
+            addMessage(doc.data(), doc.id);
+        }
     })
 })
